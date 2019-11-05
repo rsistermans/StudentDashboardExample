08010f258bb0529f0850ee65a113d07833689d72
Niels Bom <niels@nielsbom.com>
2019-11-05 16:44:10 +0100

    Add example Graphs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ChartExample from "../ChartExample/ChartExample";
 
 function App() {
   return (
     <div className="App">
       <h1>Student Dashboard</h1>
+      <ChartExample />
     </div>
   );
 }
